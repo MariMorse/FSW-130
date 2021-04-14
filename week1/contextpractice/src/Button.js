a7aa7363e7bd25c7364129107f4d79137182bdd0
MariMorse <marianne.morse@bryanuniversity.edu>
2021-04-14 12:30:14 -0400

    commit

--- week1/contextpractice/src/Button.js
+++ week1/contextpractice/src/Button.js
@@ -3,7 +3,9 @@ import React, {Component} from "react"
 class Button extends Component {
     render() {
         return (
-            <button className="light-theme">Dark Mode or Light Mode</button>
+            <button className="light-theme">Change Themes</button>
         )
     }
 }
+
+export default Button
