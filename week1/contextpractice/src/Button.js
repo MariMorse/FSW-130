6848b331a76c6e5dcfdba51a25580f74fb8b1868
MariMorse <marianne.morse@bryanuniversity.edu>
2021-04-14 12:25:07 -0400

    commit

---
+++ week1/contextpractice/src/Button.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react"
+
+class Button extends Component {
+    render() {
+        return (
+            <button className="light-theme">Dark Mode or Light Mode</button>
+        )
+    }
+}
---
+++ week1/contextpractice/src/Header.js
@@ -0,0 +1,11 @@
+import React, {component} from "react"
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="light-theme">
+                <h2>Light Theme</h2>
+            </header>
+        )
+    }
+}
