a67a7b60cac3433fc1e2cc75cacd4c88cb4b0a2a
MariMorse <marianne.morse@bryanuniversity.edu>
2021-04-24 21:50:19 -0400

    commit

--- week2/contextpractice2/src/index.js
+++ week2/contextpractice2/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import {AnimalContextProvider} from "./animalContext"
 import App from './apps/App'
 
 
 ReactDOM.render(
-
+    <AnimalContextProvider>
+         <App />
+     </AnimalContextProvider>, 
+     document.getElementById('root')
 )
