9dcc49c59fed5e2a4ae63de204d66de88d073b5c
MariMorse <marianne.morse@bryanuniversity.edu>
2021-06-03 20:56:28 -0400

    commit

--- week7/capstone-project/src/App.js
+++ week7/capstone-project/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddTodoForm from './components/AddTodoForm';
-import TodoList from './components/TotalCompleteItems';
+import TodoList from './components/TodoList';
 import TotalCompleteItems from './components/TotalCompleteItems';
 
 const App = () => {
